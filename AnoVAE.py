b30584720aa66e86f23968755a43840bf1225682
awotere <blueteresa@gmail.com>
2019-12-03 06:11:34 +0900

    npの結合が違った

--- AnoVAE.py
+++ AnoVAE.py
@@ -404,7 +404,7 @@ class AnoVAE:
 
         # predict
 
-        X_reco = []
+        X_reco = np.array([])
 
         # z取得
         _, _, z_list = encoder.predict(X_true)
@@ -416,7 +416,7 @@ class AnoVAE:
             z = np.reshape(z, (1,-1))
             x_true =  np.reshape(x_true[0], (1,-1))
             x_reco = decoder.predict([x_true,z])
-            X_reco = np.concatenate(X_reco,np.reshape(x_reco,(-1)))
+            X_reco = np.hstack(X_reco,np.reshape(x_reco,(-1)))
 
         print("再構成完了しました")
 
