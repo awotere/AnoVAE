c8fcff7c1ff41fb65903f7e528be603b6fc04e85
awotere <blueteresa@gmail.com>
2019-12-29 19:19:04 +0900

    ピーク修正

--- AnoVAE.py
+++ AnoVAE.py
@@ -579,11 +579,11 @@ class AnoVAE:
         from scipy.signal import find_peaks
         pred = [False] * len(eg)
         h = self.THRESHOLD_EG  # 最低ピーク値
-        d = int(G.TIMESTEPS * 0.5)  # ピーク同士の距離の最小値
-        wlen = G.TIMESTEPS #
+        d = int(G.TIMESTEPS * 1)  # ピーク同士の距離の最小値
+        #wlen = G.TIMESTEPS
 
         # ピーク検出
-        peaks, properties = find_peaks(eg, height=h, distance=d, wlen=wlen, prominence=prominence)
+        peaks, properties = find_peaks(eg, height=h, distance=d, prominence=prominence)
 
         # Error特定(ピークの左端を利用する)
         peak_x_list = []
